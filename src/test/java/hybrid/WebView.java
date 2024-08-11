1d748094be258985c4b02e802e1a89e636142ed3
a-levitt <artemis.levitt@gmail.com>
2024-08-11 15:08:39 +0200

    Automate switching contexts and use web-locators in hybrid app

--- src/test/java/hybrid/WebView.java
+++ src/test/java/hybrid/WebView.java
@@ -3,6 +3,7 @@ package hybrid;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
@@ -10,6 +11,8 @@ import org.openqa.selenium.remote.RemoteWebElement;
 import java.awt.*;
 import java.net.MalformedURLException;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Set;
 
 public class WebView {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
@@ -26,7 +29,27 @@ public class WebView {
         ));
 
         driver.findElement(AppiumBy.accessibilityId("WebView")).click();
-        Thread.sleep(3000);
 
+        //System.out.println(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"This page is a Second Selenium sandbox\"]")).getText());
+
+        Set<String> contextHandles = ((AndroidDriver) driver).getContextHandles();
+        for (String handle : contextHandles) {
+            System.out.println(handle);
+        }
+
+        ((AndroidDriver) driver).context("WEBVIEW");
+        // May not work because getContextHandles() often get not all contextHandles:
+        //((AndroidDriver) driver).context(contextHandles.toArray()[1].toString());
+
+        String header = driver.findElement(AppiumBy.xpath("//h1[text()='This page is a Selenium sandbox']")).getText();
+        System.out.println(header);
+        WebElement field = driver.findElement(AppiumBy.xpath("//input[@id=\"i_am_a_textbox\"]"));
+        field.clear();
+        field.sendKeys("alevitt");
+
+        ((AndroidDriver) driver).context("NATIVE_APP");
+        //((AndroidDriver) driver).context(contextHandles.toArray()[0].toString());
+
+        Thread.sleep(5000);
     }
 }
